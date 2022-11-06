f6e5922250a75d05812a94efe921d7f21be4b95a
Oluwatosin Oduwole <david2tosin@gmail.com>
2022-11-06 23:50:32 +0100

    Added SEO

--- src/CounterOne.jsx
+++ src/CounterOne.jsx
@@ -1,40 +1,47 @@
 import useCounter from "./useCounter"
 import { Link } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
+import { Helmet } from "react-helmet-async";
 
 
 const CounterOne = () => {
   const [value,increment,decrement,reset, setValue] = useCounter()
 
   return (
-    <div className="card">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Tosin</h1>
-      <p className="about">Simple Counter using a custom hook</p>
+    <>
+      <Helmet>
+        <title>{`Count is {value}`}</title>
+        <meta name="description" content="Counter using custom hook" />
+      </Helmet>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React + Tosin</h1>
+        <p className="about">Simple Counter using a custom hook</p>
 
-      <h2>Count is {value}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={() => setValue(value * 2)}>Double Count</button>
-      <p>
-        <Link to="countertwo" className="link">
-          Go to UseReducer Counter
-        </Link>
-      </p>
-      <p>
-        <Link to="buggycounter" className="link">
-          Go to Buggy Counter
-        </Link>
-      </p>
-    </div>
+        <h2>Count is {value}</h2>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={() => setValue(value * 2)}>Double Count</button>
+        <p>
+          <Link to="countertwo" className="link">
+            Go to UseReducer Counter
+          </Link>
+        </p>
+        <p>
+          <Link to="buggycounter" className="link">
+            Go to Buggy Counter
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
 export default CounterOne
--- src/CounterTwo.jsx
+++ src/CounterTwo.jsx
@@ -1,6 +1,8 @@
 import { useReducer } from "react";
 import { Link } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
+import { Helmet } from "react-helmet-async";
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -21,35 +23,45 @@ const CounterTwo = () => {
   const [state, dispatch] = useReducer(reducer, { value: 0 });
 
   return (
-    <div className="card">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <Helmet>
+        <title>{`Count is {value}`}</title>
+        <meta name="description" content="Counter using UseReducer" />
+      </Helmet>
+      <div className="card">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React + Tosin</h1>
+        <p className="about">Simple Counter using UseReducer</p>
+
+        <h2>Count is {state.value}</h2>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+        <button onClick={() => dispatch({ type: "triple" })}>X3</button>
+
+        <p>
+          <Link to="/" className="link">
+            Go to Custom Hook Counter
+          </Link>
+        </p>
+        <p>
+          <Link to="buggycounter" className="link">
+            Go to Buggy Counter
+          </Link>
+        </p>
       </div>
-      <h1>Vite + React + Tosin</h1>
-      <p className="about">Simple Counter using UseReducer</p>
-
-      <h2>Count is {state.value}</h2>
-      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
-      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-      <button onClick={() => dispatch({ type: "triple" })}>X3</button>
-
-      <p>
-        <Link to="/" className="link">
-          Go to Custom Hook Counter
-        </Link>
-      </p>
-      <p>
-        <Link to="buggycounter" className="link">
-          Go to Buggy Counter
-        </Link>
-      </p>
-    </div>
+    </>
   );
 };
 export default CounterTwo;
